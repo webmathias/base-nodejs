7821138c52187bb09d6b6eee00748f84f5da527d
Mathias H. Weber <webmathias@gmail.com>
2018-07-29 14:31:52 -0400

    Adding promise test on mocha and sample pending test

--- tests/ex1.js
+++ tests/ex1.js
@@ -10,4 +10,12 @@ describe("NodeTests", () => {
             done()
         })        
     }).timeout(2000)
+    it("Ex1", () => {
+        const NodeTests = require("../src/ex1")
+        return NodeTests.get().then(data => {
+            assert(data.id == 1, data)
+        })        
+    }).timeout(2000)
+    
+    it("Pending implementation")
 })
